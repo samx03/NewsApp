5597edb093006b11153d211b5ecb970ade6c34b0
samx03 <sameerbakshi34@gmail.com>
2022-06-19 00:38:13 +0530

    Added loading spinners

--- src/components/News.js
+++ src/components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Newsitem from './Newsitem'
+import Spinner from './Spinner';
 
 export default class News extends Component {
 
@@ -15,32 +16,45 @@ export default class News extends Component {
 
   async componentDidMount() {
     console.log("cdm");
-    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=eed3e87a3e12452f9bf3a218d75cb8e6";
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=eed3e87a3e12452f9bf3a218d75cb8e6&page=1&pageSize=${this.props.pageSize}`;
+    this.setState({
+      loading: true
+    })
     let data = await fetch(url);
     let parsedData = await data.json();
     console.log(parsedData);
-    this.setState({ articles: parsedData.articles })
+    this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults, loading: false })
   }
 
-  handleNextClick = async() => {
+  handleNextClick = async () => {
     console.log("Next");
-    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=eed3e87a3e12452f9bf3a218d75cb8e6&page=${this.state.page + 1}`;
-    let data = await fetch(url);
-    let parsedData = await data.json();
-    this.setState({
-      page: this.state.page + 1,
-      articles: parsedData.articles
-    })
+    if (!(this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize))) {
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=eed3e87a3e12452f9bf3a218d75cb8e6&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+      this.setState({
+        loading: true
+      })
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      this.setState({
+        page: this.state.page + 1,
+        articles: parsedData.articles,
+        loading: false
+      })
+    }
   }
 
-  handlePreviousClick = async() => {
+  handlePreviousClick = async () => {
     console.log("Previous");
-    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=eed3e87a3e12452f9bf3a218d75cb8e6&page=${this.state.page - 1}`;
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=eed3e87a3e12452f9bf3a218d75cb8e6&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+    this.setState({
+      loading: true
+    })
     let data = await fetch(url);
     let parsedData = await data.json();
     this.setState({
       page: this.state.page - 1,
-      articles: parsedData.articles
+      articles: parsedData.articles,
+      loading: false
     })
   }
 
@@ -48,9 +62,10 @@ export default class News extends Component {
     console.log("render");
     return (
       <div className='container my-3'>
-        <h2>Top Headlines.</h2>
+        <h2 className='text-center'>Top Headlines</h2>
+        {this.state.loading && <Spinner />}
         <div className="row">
-          {this.state.articles.map((element) => {
+          {!this.state.loading && this.state.articles.map((element) => {
             return <div key={element.url} className="col-md-4">
               <Newsitem title={element.title ? element.title.slice(0, 45) : ""} description={element.description ? element.description.slice(0, 88) : ""} imageUrl={element.urlToImage} newsUrl={element.url} />
             </div>
@@ -58,7 +73,7 @@ export default class News extends Component {
         </div>
         <div className="container d-flex justify-content-center">
           <button disabled={this.state.page <= 1} type="button" className="btn btn-dark mx-3" onClick={this.handlePreviousClick}>&larr; Previous</button>
-          <button type="button" className="btn btn-dark mx-3" onClick={this.handleNextClick}>Next &rarr;</button>
+          <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} type="button" className="btn btn-dark mx-3" onClick={this.handleNextClick}>Next &rarr;</button>
         </div>
       </div>
     )
